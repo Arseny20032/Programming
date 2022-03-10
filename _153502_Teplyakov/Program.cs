17c36ac20a4f62e27ccda096c8143878f7d024b0
Арсений Тепляков <teplyakovarseni@mail.ru>
2022-03-10 12:23:25 +0300

    Добавьте файлы проекта.

---
+++ _153502_Teplyakov/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a / b;
+
+Console.WriteLine($"Частное от деления чисел: {c}");
+
